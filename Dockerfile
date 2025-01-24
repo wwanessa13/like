186aa95dedc09ac6c25b26f3c8de2ea30b345f80
Wanessa Lima <128439772+wwanessa13@users.noreply.github.com>
2025-01-24 15:30:56 -0600

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM rstudio/plumber:latest
+
+COPY exemplo_api.R /exemplo_api.R
+
+EXPOSE 8000
+
+CMD ["Rscript", "/exemplo_api.R"]
---
+++ exemplo_api.R
@@ -0,0 +1,12 @@
+library(plumber)
+
+#* Falar que x é legal
+#* @param x O que você quer que seja dito que é legal
+#* @get /mensagem
+
+function(x){
+  print(paste0(x, "é legal"))
+}
+
+exe <- plumb('exemplo_api.R')
+exe$run(host = '0.0.0.0', port = 8000)
